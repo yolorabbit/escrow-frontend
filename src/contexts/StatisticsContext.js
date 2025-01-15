8455f50682f900fa6b2be8539e7851759ebd73a9
YOLORabbit <surprise.eng000@gmail.com>
2025-01-15 11:31:09 +0900

    fix; revert things

--- src/contexts/StatisticsContext.js
+++ src/contexts/StatisticsContext.js
@@ -129,11 +129,11 @@ export const StatisticsProvider = ({ children }) => {
                 escrowBalance != statistics.escrowBalance 
             ) {
                 setStatistics({
-                    usdtBalance: usdtBalance == 0? statistics.usdtBalance: usdtBalance,
-                    accumulatedFees: accumulatedFees == 0? statistics.accumulatedFees: accumulatedFees,
-                    escrowCount: escrowCount == 0? statistics.escrowCount: escrowCount,
-                    platformFee: platformFee == 0? statistics.platformFee: platformFee,
-                    escrowBalance: escrowBalance == 0? statistics.escrowBalance: escrowBalance
+                    usdtBalance,
+                    accumulatedFees,
+                    escrowCount,
+                    platformFee,
+                    escrowBalance
                 });
             }
 
@@ -147,7 +147,7 @@ export const StatisticsProvider = ({ children }) => {
             const tronWeb = window.tron.tronWeb;
             
             const escrows_ = await getEscrows(tronWeb);
-            if (escrows_.length != escrows.length && escrows_.length > 0) {
+            if (escrows_.length != escrows.length) {
                 setEscrows(escrows_);
             }
 
